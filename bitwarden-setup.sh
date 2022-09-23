b2d6dc5047a5030d22b1f6d2208d9cd4c7054c5a
Robert Helgeson <son_of_helga@icloud.com~>
2022-09-23 12:12:29 -0700

    initial try

--- bitwarden-setup.sh
+++ bitwarden-setup.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+echo "do not run this bad boy as root."
+echo "what is your username?"
+read name
+
+sudo apt udpate -y && sudo apt full-upgrade
+sudo apt install apache2 curl
+sudo apt install apt-transport-https ca-certificates gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce docker-compose
+sudo usermod -aG docker $name
+curl -Lso bitwarden.sh https://go.btwrdn.co/bw-sh && chmod 700 bitwarden.sh
+chmod +x bitwarden.sh 
+sudo apt install certbot python-certbot-apache
+sudo apt install certbot python3-certbot-apache
+certbot --apache -d www.theitadventure.com
+sudo certbot --apache -d www.theitadventure.com
+sudo systemctl disable apache2
+./bitwarden install
